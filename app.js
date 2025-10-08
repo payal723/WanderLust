ce589013df0ff46bb0e069b2abc986ee4e1e8f78
payal723 <payaljaat55@gmail.com>
2025-10-09 00:43:45 +0530

    fix: use dynamic port for Render deployment

--- app.js
+++ app.js
@@ -278,7 +278,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 10000;
 
 server.listen(PORT, () => {
   console.log(`server is listening to port ${PORT}`);
